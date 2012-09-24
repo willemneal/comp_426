6815007651df6d997b21e7f9bde0c562d6fd1f18
Willem Wyndham <willem.neal@gmail.com>
2012-09-24 11:49:03 -0400

    added js objects

---
+++ Article.js
@@ -0,0 +1,24 @@
+/* News Article
+ * 
+ * Contains info about each article
+ * including: date, genre, title, 
+ * author, location, source url.
+ * 
+ * example: var = new Article('URL',',"UNC Wins', date,
+  'author', 'sports', 'Chapel Hill, NC')
+ * 
+ */
+ var Article = function (source,title, date, 
+ 						author, genre, location) {
+ 	this.source = source;
+ 	this.title = title;
+ 	this.date = date;
+ 	this.author = author;
+ 	this.genre = genre;
+ 	this.location = location;
+ }
+ Article.DAYINMS = 86400000;
+ Article.prototype.isOld = function() {
+ 	return this.date + 3 * this.DAYINMS < this.date.getTime();
+ }
+*/
