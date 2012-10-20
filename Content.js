55cf19fc7798d1bdf53dc88a670671ce821ef0ab
Willem Wyndham <willem.neal@gmail.com>
2012-10-20 13:48:13 -0400

    Added Articles
    I added Content and edited index_empty and commented jsonFlickrApi.
    Something to fix later is that when there is a search with nothing the
    page goes empty with just fail at the top. Simple to fix, but just
    haven't.

--- Content.js
+++ Content.js
@@ -1,45 +1,66 @@
 /* Content
  * 
- * Contains info about each article
- * including: date, genre, title, 
- * author, location, source url.
- * 
- * example: var = new Article('URL',',"UNC Wins', date,
-  'author', 'sports', 'Chapel Hill, NC')
- * 
  */
- var Article = function (source,title, date, 
- 						author, genre, location,preview) {
- 	this.source = source;
- 	this.title = title;
- 	this.date = date;
- 	this.author = author;
- 	this.genre = genre;
- 	this.location = location;
- 	this.preview = preview;
- }
- Article.DAYINMS = 86400000;
- Article.prototype.isOldThan = function(day) {//returns whether article is old.
- 	return this.date + day * this.DAYINMS < this.date.getTime();
- }
-
 
 Article.all = [];
 Article.push(new Article('http://www.theonion.com/articles/
 	obama-excited-to-participate-in-first-debate,29930/?ref=auto',"Obama Excited To Participate In First Debate",
-	new date(10,15,2012),"n/a","politics","Washington","Saying he was excited to “finally get out there” and defend his policies in front of the entire nation, President Barack Obama told reporters he was energized and eager to participate in his first debate of the 2012 election cycle Tuesday night."));
+	new Date(2012,10,15),"n/a","politics","Washington","Saying he was excited to “finally get out there” and defend his policies in front of the entire nation, President Barack Obama told reporters he was energized and eager to participate in his first debate of the 2012 election cycle Tuesday night."));
 Article.push(new Article("http://www.theonion.com/articles/polls-reveal-essentially-nothing,29927/?ref=auto",
-	"Polls Reveal, Essentially, Nothing",new date(10,15,2012),"n/a",
+	"Polls Reveal, Essentially, Nothing",
+	new Date(2012,10,15),
+	"n/a","politics",
+	"Washington",
 	"A slew of recently released polls from the nation’s top public opinion
 	 firms have revealed, well, essentially nothing about the 2012 presidential
 	  race, sources confirmed Monday. Conducted by Ipsos, Gallup, Public Policy Polling,
 	   and Rasmussen, the pretty much meaningless polls reportedly offered no deep insight
 	    into any key demographic in the United States and, when it comes down to it, were all 
 	    basically pointless and a waste of everyone’s time.");
-Article.push(new Article(
-Article.push(new Article(
-Article.push(new Article(
-Article.push(new Article(
-Article.push(new Article(
-Article.push(new Article(
-Article.push(new Article(
+Article.push(new Article("http://www.theonion.com/articles/i-feel-your-pain-romney-tells-campaign-rally-atten,29929/",
+	"'I Feel Your Pain,' Romney Tells Campaign Rally Attendees Who Make $20 Million A Year",
+	new Date(2012,10,20),
+	"n/a", "politics",
+	"INDIAN HILL, OH", "As he continues to tout his plan to fix the economy and “make things right,” presidential nominee Mitt Romney delivered a heartfelt address to wealthy rally attendees Monday, telling those who make more than $20 million per year that he “feels [their] pain” and can relate to their struggles"));
+Article.push(new Article("http://www.theonion.com/articles/scientists-working-on-immortality-better-hurry-up,29670/",
+	"Scientists Working On Immortality Better Hurry Up Because Ian McKellen Is 73",
+	new Date(2012,9,24),
+	"n/a",
+	"technology",
+	"Washington", 
+	"Scientists in search of a cure for human mortality better pick up the pace, sources confirmed this week, because celebrated British actor Sir Ian McKellen is not exactly getting any younger. McKellen, a beloved star of stage and screen, is reportedly a mortal 73-year-old human being and, as such, will be lost to the world within decades unless immortality researchers start showing a little more hustle. Indeed, sources added, if scientists don’t get to work tout de suite on some kind of everlasting-life serum, movie scripts calling for aged and wizened mentor figures will soon face a noticeably weaker casting field. According to reports, any neuroscientist or molecular geneticist working on the project and lacking motivation is encouraged to watch a critically lauded performance from McKellen’s extensive filmography or, if possible, see him on the stage as King Lear or Richard III. At press time, researchers claimed they were nearing a breakthrough, but before proceeding further hoped to wait until actor and director Woody Allen, 76, had passed away."));
+Article.push(new Article("http://www.theonion.com/articles/new-roomba-blender-makes-smoothie-out-of-everythin,29539",
+	"New Roomba Blender Makes Smoothie Out Of Everything In Its Path", 
+	new Date(2012,9,13),
+	"n/a", "technology", 
+	"BEDFORD, MA",
+	"In an exciting extension of their product line, the creators of the Roomba-brand self-directed vacuum cleaner have introduced the Yumba, a new household blender capable of mixing a rich, frothy smoothie out of whatever ingredients it sucks up from your floor. “With the Yumba, you can make a banana, floor sweepings, and wheat germ shake; a strawberry-mango-cigarette-butt frappé; or, for a light lunch, a zesty toenail-clipping-cat-hair-dead-skin-and-pineapple smoothie,” product developer Peter Olsen said Monday, demonstrating the new device on a carpet covered in sawdust, garden vegetables, and detritus. “Mmm, you can taste the cobwebs.” When asked if the Yumba could make smoothies out of pennies and melon, Haskins said that it could."));
+Article.push(new Article("http://www.theonion.com/articles/new-apple-campaign-urges-consumers-to-buy-iphone-f,28934/?ref=auto",
+	"New Apple Campaign Urges Consumers To Buy iPhone For Other Hand",
+	new Date(2012,7,26),
+	"n/a",
+	"technology",
+	"CUPERTINO, CA",
+	"As part of an effort to boost smartphone sales following lower-than-expected third-quarter earnings, Apple announced today a new national advertising campaign urging users to buy a second iPhone for their other hand. \"Imagine having Safari, FaceTime, and Siri in both of your hands at the same time,\" Apple CEO Tim Cook said in a press release for the new campaign, which will debut Friday night during the Olympic opening ceremonies with the slogan \"Need an extra hand with that?\" \"On the one hand—quite literally—you're texting a friend, and on the other, you're hitting up Siri for the nearest pizza joint. With that second phone, iPhone users will truly have the power of the Internet at their fingertips—all 10 of them. This is the wave of the future right here.\" At press time, Apple reported that iPhone sales for the day had passed the 600,000-order mark."));
+Article.push(new Article("http://www.theonion.com/articles/smug-replacement-refs-point-out-not-a-single-inten,29784/",
+	"Smug Replacement Refs Point Out Not A Single 'Intentional Midfield Zone Hands' Called All Weekend",
+	new Date(2012,10,3),
+	"n/a",
+	"sports",
+	"NEW YORK",
+	"Following the return of the NFL’s regular officials, several smug replacement referees told reporters Tuesday about the abundance of missed calls last week, noting that since their dismissal not a single flag has been thrown for intentional midfield zone hands, personal clock interference, or roughing the line of scrimmage."));
+Article.push(new Article("http://www.theonion.com/articles/report-jack-blacks-life-more-valuable-than-yours-i,29919/",
+	"Report: Jack Black's Life More Valuable Than Yours If It Ever Comes Down To It",
+	new Date(2012,10,15),
+	"n/a",
+	"entertainment",
+	"ITHACA, NY",
+	"According to a new study by Cornell University, in any conceivable scenario in which a person would have to choose between saving your life or that of actor-comedian Jack Black, the Shallow Hal star would live and you would die. “In countless hypothetical ‘lifeboat’ scenarios, our research indicates that you are ultimately more expendable, because when it comes down to it, Jack Black’s life is more important to people than your own,” the study’s lead author Dr. Thomas Malden told reporters Monday, adding that if, for example, the world were coming to an end and there were one space left in an underground bunker, the actor who appeared in The Holiday would be on a government “save” list, and you would have to fend for yourself."));
+Article.push(new Article("http://www.theonion.com/articles/tommy-lee-jones-tells-jimmy-fallon-he-doesnt-want,29731/",
+	"Tommy Lee Jones Tells Jimmy Fallon He Doesn't Want To Play Any Of His Little Fucking Games",
+	new Date(2012,9,28),
+	"n/a",
+	"entertainment",
+	"NEW YORK",
+	"Prior to his appearance on Late Night With Jimmy Fallon last week, actor Tommy Lee Jones informed the host that he had no intention of playing “any of [his] little fucking games,” according to sources at NBC. “Listen here, Howdy Doody, I’m not putting on a wig, I’m not doing your skits, I’m not reading any Mötley Crüe lyrics, and I’m sure as shit not singing the Men In Black song with your goddamn band,” said the Oscar winner, who was on the show to promote his film Hope Springs. \"Here’s what’s gonna happen. I’m gonna walk out there, sit down, and tell you an amusing anecdote about my movie. Then you’re gonna roll the clip, thank me, and have a producer walk me to the elevator. That’s it. Nothing else. Zippo. I’m a 66-year-old man, for Christ’s sake. So you and your giddy little writing staff can just calm the fuck down.\""));
+Aricle.push(new Aricle()
--- jsonFlickrApi.js
+++ jsonFlickrApi.js
@@ -35,7 +35,10 @@ function jsonFlickrApi(rsp) {
 
 
 $(document).ready(function () {
+	//Function to get JSON from flickr webservices
 	$.getJSON("http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=805470a8b0ea366a9acbc219cbe895cb&tags=University+of+North+Carolina+at+Chapel+Hill%2CUNC%2CBasketball%2Ctarheel%2Cdeandome&sort=interestingness-desc&format=json&jsoncallback=?",jsonFlickrApi)
+	
+	//Event for the form under picture so that user can search pictures
 	$('#the_form').on('submit', function(e) {
 		var s = $('#the_form input[name="itemval"]').val().split(' ').join('+').split(',').join("%2C");
 		//alert(s);
